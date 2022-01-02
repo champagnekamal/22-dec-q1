ab2ac1f7a49d768eb4ca6d7788946a4097890b7e
champagnekamal <akashkamal70@gmail.com>
2022-01-02 21:01:52 +0530

    first

---
+++ q1.js
@@ -0,0 +1,8 @@
+let arr = ["hi", "akash", "kamal"];
+
+arr.push(promt("enter a element"));
+arr.push(prompt("enter second element"));
+arr.push(prompt("enter third element"));
+arr.push(prompt("enter fourth element"));
+
+console.log(arr);
